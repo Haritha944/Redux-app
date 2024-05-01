05b3615f4de5fcaaf890fe3f97e8ad670ea34c75
Haritha944 <harithams23@gmail.com>
2024-05-01 05:46:59 +0530

    urls and view

---
+++ Backend/webapp/myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import UserRegister,UserList,UserDetails,MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('token/',MyTokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(),name='token_refresh'),
+    path('user-list/',UserList.as_view(),name='user_list'),
+    path('user_detail/<int:id>/',UserDetails.as_view(),name='user_details'),
+    path('user-register/',UserRegister.as_view(),name="user_register"),
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Backend/webapp/myapp/views.py
+++ Backend/webapp/myapp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .serializers import UserSerializer,myTokenObtainPairSerailizer
+from .models import UserAccount
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.filters import SearchFilter
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = myTokenObtainPairSerailizer
+
+class UserRegister(CreateAPIView):
+    serializer_class = UserSerializer
+class UserList(ListCreateAPIView):
+    queryset = UserAccount.objects.all().exclude(is_superuser=True)
+    serializer_class=UserSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['email','first_name']
+
+class UserDetails(RetrieveUpdateDestroyAPIView):
+    queryset = UserAccount.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'id'
+    print("-------view-------")
+    def get_object(self):
+        user_id = self.kwargs.get('id')
+        user = get_object_or_404(UserAccount,id=user_id)
+        print("-------",user)
+        return user
+    def perform_update(self, serializer):
+        serializer.save()
+
